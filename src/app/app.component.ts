3081eb3955732239056aad84487f091de0cd06cf
hyungkawmld <moulahidkawtar@email.com>
2023-01-03 16:20:03 +0100

    Getting start with observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { filter, map, Observable, tap } from 'rxjs';
+import { interval } from 'rxjs/internal/observable/interval';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
+
+  interval$! : Observable<string>;
+
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+   this.interval$ = interval(1000).pipe(
+    filter(value => value % 3 === 0),
+    map(value => value % 2 === 0 ?
+      `je suis ${value} et je suis par` :
+      `je suis ${value} et je suis impair`
+      ),
+    tap(text => this.logger(text))
+    );
+    
+   
+  }
+  logger(text: string){
+    console.log(`log: ${text}`);
   }
  
 }
