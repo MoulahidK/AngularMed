5c2bd77f800c378bd3743f3cfbe45abf33df9f5d
hyungkawmld <moulahidkawtar@email.com>
2023-01-07 16:56:26 +0100

    Add Observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { filter, map, Observable, tap } from 'rxjs';
+import { delay, filter, map, mergeMap, Observable, of, take, tap } from 'rxjs';
 import { interval } from 'rxjs/internal/observable/interval';
 
 @Component({
@@ -9,22 +9,34 @@ import { interval } from 'rxjs/internal/observable/interval';
 })
 export class AppComponent implements OnInit{
 
-  interval$! : Observable<string>;
-
+  redTrainsCalled = 0;
+  yellowTrainsCalled = 0;
+  
   ngOnInit(): void {
-   this.interval$ = interval(1000).pipe(
-    filter(value => value % 3 === 0),
-    map(value => value % 2 === 0 ?
-      `je suis ${value} et je suis par` :
-      `je suis ${value} et je suis impair`
-      ),
-    tap(text => this.logger(text))
-    );
+   interval(500).pipe(
+    take(0),
+    map(value => value % 2 === 0 ? 'rouge': 'jaune'),
+    tap(color => console.log(`la lumiere s'allume en %c${color}`, `color:
+    ${this.translateColor(color)}`)),
+    mergeMap(color => this.getTrainObservable$(color)),
+    tap(train => console.log(`Train %c${train.color} ${train.trainIndex} arrivé !`, `font-weight: bold; color: ${this.translateColor(train.color)}`))
+  ).subscribe();
+}
+
+getTrainObservable$(color: 'rouge' | 'jaune') {
+  const isRedTrain = color === 'rouge';
+  isRedTrain ? this.redTrainsCalled++ : this.yellowTrainsCalled++;
+  const trainIndex = isRedTrain ? this.redTrainsCalled : this.yellowTrainsCalled;
+  console.log(`Train %c${color} ${trainIndex} appelé !`, `text-decoration: underline; color: ${this.translateColor(color)}`);
+  return of({ color, trainIndex }).pipe(
+    delay(isRedTrain ? 5000 : 6000)
+  );
+}
+
+translateColor(color: 'rouge' | 'jaune') {
+  return color === 'rouge' ? 'red' : 'yellow';
+}
     
    
   }
-  logger(text: string){
-    console.log(`log: ${text}`);
-  }
- 
-}
+  
