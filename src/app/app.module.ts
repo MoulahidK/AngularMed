c7d90026abbffd571322b200797da623f99fdc4b
hyungkawmld <moulahidkawtar@email.com>
2023-01-21 15:50:10 +0100

    Add a start point towards modularization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { registerLocaleData } from '@angular/common';
 
-import * as fr from '@angular/common/locales/fr';
+
 
 import { AppComponent } from './app.component';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
 import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
-import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { SingleFaceSnapComponent } from './single-face-snap/single-face-snap.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NewFaceSnapComponent } from './new-face-snap/new-face-snap.component';
-import { httpInterceptorProviders } from './interceptors';
+import { CoreModule } from './core/core.module';
+
 
 
 @NgModule({
@@ -22,7 +22,6 @@ import { httpInterceptorProviders } from './interceptors';
     AppComponent,
     FaceSnapComponent,
     FaceSnapListComponent,
-    HeaderComponent,
     LandingPageComponent,
     SingleFaceSnapComponent,
     NewFaceSnapComponent
@@ -32,17 +31,12 @@ import { httpInterceptorProviders } from './interceptors';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    CoreModule
   ],
-  providers: [
-   { provide: LOCALE_ID, useValue: 'fr-FR' },
-   httpInterceptorProviders
-
-  ],
+  
   bootstrap: [AppComponent]
 })
 export class AppModule { 
-  constructor(){
-    registerLocaleData(fr.default);
-  }
+  
 }
